d1eb0711cf98f8166c7430de8bf387b7c1e6eeee
VitorLC-unipam <vitorlc@unipam.edu.br>
2025-08-26 01:08:36 +0000

    aula04

---
+++ banco01/schema.sql
@@ -0,0 +1,42 @@
+PRAGMA foreing_keys = ON;
+
+CREATE TABLE usuario(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    senha TEXT NOT NULL
+);
+
+CREATE TABLE cliente(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL,
+    telefone TEXT,
+    usuario_id INTEGER NOT NULL UNIQUE,
+    FOREIGN KEY (usuario_id) REFERENCES usuario(id) ON DELETE CASCADE
+);
+
+CREATE TABLE produto(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL,
+    descricao TEXT,
+    preco REAL NOT NULL CHECK(preco >= 0)
+);
+
+CREATE TABLE venda(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    data TEXT NOT NULL,
+    usuario_id INTEGER NOT NULL,
+    cliente_id INTEGER NOT NULL,
+    FOREIGN KEY (cliente_id) REFERENCES cliente(id) ON DELETE SET NULL,
+    FOREIGN KEY (usuario_id) REFERENCES usuario(id) ON DELETE SET NULL
+);
+
+CREATE TABLE venda_produto(
+    venda_id INTEGER NOT NULL,
+    produto_id INTEGER NOT NULL,
+    quantidade INTEGER NOT NULL CHECK (quantidade > 0),
+    preco_unitario REAL NOT NULL CHECK (preco_unitario >= 0),
+    PRIMARY KEY (venda_id, produto_id),
+    FOREIGN KEY (venda_id) REFERENCES venda(id) ON DELETE CASCADE,
+    FOREIGN KEY (produto_id) REFERENCES produto(id) ON DELETE CASCADE
+); 
